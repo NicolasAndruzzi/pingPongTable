57a9480e5d7ca01cf453969407c788ab76497007
Nicolas Andruzzi <NicolasAndruzzi@gmail.com>
2016-04-05 17:20:12 -0600

    migrations working in development and production environments

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,13 @@
+require('dotenv').load();
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgres://localhost/pingPongTable'
   },
-
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL + '?ssl=true'
   }
 
 };
