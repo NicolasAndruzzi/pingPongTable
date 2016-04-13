aad4b02e6b58a8f8ff48e4d26859c0f95612245a
Nicolas Andruzzi <NicolasAndruzzi@gmail.com>
2016-04-13 14:04:00 -0600

    showing scores to users

--- public/javascripts/controllers/onTheTableController.js
+++ public/javascripts/controllers/onTheTableController.js
@@ -2,5 +2,8 @@
 app.controller("onTheTableController", function ($scope, socket) {
   socket.on("scores", function (scores) {
     console.log(scores)
+    $scope.playerOneScore = scores.playerOneScore;
+    $scope.playerTwoScore = scores.playerTwoScore;
+    $scope.$digest()
   })
 })
